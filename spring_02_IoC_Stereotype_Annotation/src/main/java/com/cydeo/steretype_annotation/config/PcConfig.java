23ab9b69783ae0ae54f7b9eeeb3c27fb329c27bb
omrzcn <omrzcnn@gmail.com>
2024-03-19 20:30:07 +0300

    basePackages updated

--- spring_02_IoC_Stereotype_Annotation/src/main/java/com/cydeo/steretype_annotation/config/PcConfig.java
+++ spring_02_IoC_Stereotype_Annotation/src/main/java/com/cydeo/steretype_annotation/config/PcConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.cydeo.steretype_annotation")
+@ComponentScan(basePackages = "com.cydeo")
 public class PcConfig {
 
 
