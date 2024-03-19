cb6a7e4ef8ebbafd05766ee87397418603e14433
omrzcn <omrzcnn@gmail.com>
2024-03-19 15:51:50 +0300

    2 more monitor added by name and primary annotation

--- spring_01_IoC_Bean_Annotation/src/main/java/com/cydeo/bean_annotation/config/ComputerConfig.java
+++ spring_01_IoC_Bean_Annotation/src/main/java/com/cydeo/bean_annotation/config/ComputerConfig.java
@@ -2,21 +2,34 @@ package com.cydeo.bean_annotation.config;
 
 import com.cydeo.bean_annotation.casefactory.Case;
 import com.cydeo.bean_annotation.casefactory.DellCase;
+import com.cydeo.bean_annotation.monitorfactory.AcerMonitor;
 import com.cydeo.bean_annotation.monitorfactory.Monitor;
 import com.cydeo.bean_annotation.monitorfactory.SonyMonitor;
 import com.cydeo.bean_annotation.motherboardfactory.AsusMotherboard;
 import com.cydeo.bean_annotation.motherboardfactory.Motherboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ComputerConfig {
     //created a class and annotated by @Configuration
 
-    @Bean()
-    public SonyMonitor monitorSony(){
+    @Bean(name = "sony")
+    public Monitor monitorSony(){
         return new SonyMonitor("25 inc Beast","Sony",25);
     }
+
+    @Bean
+    public Monitor monitorSony2(){
+        return new SonyMonitor("40 inch Beast","Sony",40);
+
+
+    }
+
+    @Bean
+    @Primary
+    public Monitor monitorAcer(){return new AcerMonitor("23 inch Beast","Acer",23);}
 
     @Bean
    public Motherboard motherboardAsus(){
@@ -28,6 +41,9 @@ public class ComputerConfig {
 
         return new DellCase("220B","Dell","240");
     }
+
+
+
 
 
 
