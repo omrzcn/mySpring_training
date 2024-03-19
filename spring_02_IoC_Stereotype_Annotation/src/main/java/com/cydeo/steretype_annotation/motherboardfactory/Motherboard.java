72386b2fd178013d344961f904b03a91bb2c179d
omrzcn <omrzcnn@gmail.com>
2024-03-19 20:29:39 +0300

    lombok added

--- spring_02_IoC_Stereotype_Annotation/src/main/java/com/cydeo/steretype_annotation/motherboardfactory/Motherboard.java
+++ spring_02_IoC_Stereotype_Annotation/src/main/java/com/cydeo/steretype_annotation/motherboardfactory/Motherboard.java
@@ -1,5 +1,10 @@
 package com.cydeo.steretype_annotation.motherboardfactory;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public abstract class Motherboard {
     private String model;
     private String manufacturer;
