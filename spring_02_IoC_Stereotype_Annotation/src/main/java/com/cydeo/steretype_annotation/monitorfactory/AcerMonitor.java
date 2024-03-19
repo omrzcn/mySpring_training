ef7891342e6a760eb04ff8b4b1bb5ae2fa9345fc
omrzcn <omrzcnn@gmail.com>
2024-03-19 17:51:08 +0300

    Acer class created

---
+++ spring_02_IoC_Stereotype_Annotation/src/main/java/com/cydeo/steretype_annotation/monitorfactory/AcerMonitor.java
@@ -0,0 +1,12 @@
+package com.cydeo.steretype_annotation.monitorfactory;
+
+public class AcerMonitor extends Monitor{
+    public AcerMonitor() {
+        super("22 inch beast","Sony",23);
+    }
+
+    @Override
+    public void drawPixelAt() {
+        System.out.println("Drawing pixel with Acer");
+    }
+}
