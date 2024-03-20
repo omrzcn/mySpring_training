899bdf2ba01c7b7e1747bb823182a4834883f673
omrzcn <omrzcnn@gmail.com>
2024-03-20 15:16:48 +0300

    autiwired

--- spring_04_Autowiring/src/main/java/com/cydeo/streotype_annotation/ComputerTest.java
+++ spring_04_Autowiring/src/main/java/com/cydeo/streotype_annotation/ComputerTest.java
@@ -1,16 +1,21 @@
 package com.cydeo.streotype_annotation;
 
+import com.cydeo.streotype_annotation.casefactory.Case;
 import com.cydeo.streotype_annotation.config.PcConfig;
+import com.cydeo.streotype_annotation.monitorfactory.Monitor;
+import com.cydeo.streotype_annotation.monitorfactory.SonyMonitor;
+import com.cydeo.streotype_annotation.motherboardfactory.Motherboard;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ComputerTest {
     public static void main(String[] args) {
 
-        ApplicationContext container =new AnnotationConfigApplicationContext(PcConfig.class);
+        ApplicationContext container = new AnnotationConfigApplicationContext(PcConfig.class);
 
-        PC myPc = container.getBean(PC.class);
+       PC myPc = container.getBean(PC.class);
+        System.out.println(myPc.getMonitor().getManufacturer());
 
-        System.out.println(myPc.getTheCase().getDimensions().getDepth());
+
     }
 }
