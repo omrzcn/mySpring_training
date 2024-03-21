aee82ac5f9b2761a9778890b51a04509916a8d5b
omrzcn <omrzcnn@gmail.com>
2024-03-21 13:45:12 +0300

    abstraction

--- spring_05_Core_Abstraction/src/main/java/com/cydeo/service/CommentService.java
+++ spring_05_Core_Abstraction/src/main/java/com/cydeo/service/CommentService.java
@@ -4,9 +4,12 @@ import com.cydeo.model.Comment;
 import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class CommentService {
 
     private final CommentRepository commentRepository; // this interface is for saving object
