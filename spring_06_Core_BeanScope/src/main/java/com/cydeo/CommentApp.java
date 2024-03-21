d6340fc399c666cec9a5a00b5f1b778685711ee0
omrzcn <omrzcnn@gmail.com>
2024-03-21 13:30:22 +0300

    Singleton bean

---
+++ spring_06_Core_BeanScope/src/main/java/com/cydeo/CommentApp.java
@@ -0,0 +1,19 @@
+package com.cydeo;
+
+import com.cydeo.config.CommentConfig;
+import com.cydeo.model.Comment;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CommentApp {
+    public static void main(String[] args) {
+
+        Comment comment = new Comment();
+        comment.setAuthor("Johnson");
+        comment.setText("Spring Framework");
+
+        ApplicationContext container = new AnnotationConfigApplicationContext(CommentConfig.class);
+
+
+    }
+}
---
+++ spring_06_Core_BeanScope/src/main/java/com/cydeo/service/CommentService.java
@@ -0,0 +1,24 @@
+package com.cydeo.service;
+
+import com.cydeo.model.Comment;
+import com.cydeo.proxy.CommentNotificationProxy;
+import com.cydeo.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentService {
+
+    private final CommentRepository commentRepository; // this interface is for saving object
+    private final CommentNotificationProxy commentNotificationProxy; // this interface is for sending email.
+
+    public CommentService(CommentRepository commentRepository, @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
+
+    public void publishComment(Comment comment){ // we are saving and pushing Comments
+        commentRepository.storeComment(comment);
+        commentNotificationProxy.sendComment(comment);
+    }
+}
