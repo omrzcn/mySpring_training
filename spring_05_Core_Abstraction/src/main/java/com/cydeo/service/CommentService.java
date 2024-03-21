468aa941e52265e0707718041bfdbb376c2893a6
omrzcn <omrzcnn@gmail.com>
2024-03-21 13:24:52 +0300

    Abstraction

--- spring_05_Core_Abstraction/src/main/java/com/cydeo/service/CommentService.java
+++ spring_05_Core_Abstraction/src/main/java/com/cydeo/service/CommentService.java
@@ -9,15 +9,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class CommentService {
 
-    private final CommentRepository commentRepository;
-    private final CommentNotificationProxy commentNotificationProxy;
+    private final CommentRepository commentRepository; // this interface is for saving object
+    private final CommentNotificationProxy commentNotificationProxy; // this interface is for sending email.
 
-    public CommentService(CommentRepository commentRepository, @Qualifier("EMAIL") CommentNotificationProxy commentNotificationProxy) {
+    public CommentService(CommentRepository commentRepository, @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
     }
 
-    public void publishComment(Comment comment){
+    public void publishComment(Comment comment){ // we are saving and pushing Comments
         commentRepository.storeComment(comment);
         commentNotificationProxy.sendComment(comment);
     }
