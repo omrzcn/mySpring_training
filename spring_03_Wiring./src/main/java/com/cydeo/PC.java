dc066e6d8b97572428e1d51e04f2af0c4f506610
omrzcn <omrzcnn@gmail.com>
2024-03-26 16:14:19 +0300

    wiring

--- spring_03_Wiring./src/main/java/com/cydeo/PC.java
+++ spring_03_Wiring./src/main/java/com/cydeo/PC.java
@@ -1,4 +1,24 @@
 package com.cydeo;
 
+import com.cydeo.casefactory.Case;
+import com.cydeo.monitorfactory.Monitor;
+import com.cydeo.motherboardfactory.Motherboard;
+
 public class PC {
-}
+
+    private Case aCase;
+    private Monitor monitor;
+    private Motherboard motherboard;
+
+
+    public PC(Case aCase, Monitor monitor, Motherboard motherboard) {
+        this.aCase = aCase;
+        this.monitor = monitor;
+        this.motherboard = motherboard;
+    }
+
+
+
+
+
+    }
