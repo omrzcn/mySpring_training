e4f9908980d47e8853c4003c0039c919ea86ecd3
omrzcn <omrzcnn@gmail.com>
2024-04-04 20:56:33 +0300

    thymeleaf

--- spring_10_Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring_10_Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -4,6 +4,8 @@ import com.cydeo.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -19,7 +21,7 @@ public class MentorController {
 
     @GetMapping("/register") // gove me the page
     public String register(Model model){
-        List<String> bacthList = List.of("JD1,JD2,JD3");
+        List<String> bacthList = List.of("JD1","JD2","JD3");
         model.addAttribute("batchList",bacthList);
 
         model.addAttribute("mentor",new Mentor());
@@ -28,6 +30,25 @@ public class MentorController {
 
         return "mentor/mentor-register";
 
+    }
+
+    @PostMapping("/confirm") // we will use post mapping because we are posting :)
+    public String submitForm(@ModelAttribute("mentor") Mentor mentor) {
+
+
+
+
+
+
+        return "mentor/mentor-confirmation";
+    }
+    @GetMapping("/confirm") // we will use post mapping because we are posting :)
+    public String submitForm2(Model model){
+
+
+
+
+        return "mentor/mentor-confirmation";
     }
 
 
