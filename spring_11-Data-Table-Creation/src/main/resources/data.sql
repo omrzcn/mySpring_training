5e7bf2529767e84e7b50aefe07ad049183e5afe1
omrzcn <omrzcnn@gmail.com>
2024-05-01 20:08:51 +0300

    sql side table creation

--- spring_11-Data-Table-Creation/src/main/resources/data.sql
+++ spring_11-Data-Table-Creation/src/main/resources/data.sql
@@ -1,2 +1,6 @@
 INSERT INTO EMPLOYEES(id,name)
 VALUES(1,'Mike');
+
+insert into student (id,first_name,last_name,email) values (1,'Mike','Smith','mike@cydeo.com');
+insert into student (id,first_name,last_name,email) values (2,'Tom','Hanks','tom@cydeo.com');
+
