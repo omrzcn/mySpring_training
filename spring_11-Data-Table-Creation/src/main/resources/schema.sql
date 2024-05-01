dac3d8c2349957a3374cd801185a20563378e220
omrzcn <omrzcnn@gmail.com>
2024-05-01 18:18:15 +0300

    table created

---
+++ spring_11-Data-Table-Creation/src/main/resources/schema.sql
@@ -0,0 +1,10 @@
+drop table if exists employees;
+-- it means same with method name.
+
+create table EMPLOYEES(
+
+                          id bigint not null,
+                          name varchar(255),
+                          primary key (id)
+
+);
