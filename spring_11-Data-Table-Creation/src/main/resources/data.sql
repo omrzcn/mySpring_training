7adb2f6328744dda9137c782ea1e6f2629f3e3f5
omrzcn <omrzcnn@gmail.com>
2024-05-02 17:34:29 +0300

    connection

--- spring_11-Data-Table-Creation/src/main/resources/data.sql
+++ spring_11-Data-Table-Creation/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO employee(id,name)
 VALUES(1,'Mike');
 
-insert into students (first_name,last_name,email) values ('Mike','Smith','mike@cydeo.com');
--- we deleted id's. because database is gpnna create id for us.
+insert into students(first_name,last_name,email) VALUES('Mike','Smith','mike@cydeo.com');
+-- we deleted id's. because database is gonna create id for us.
 insert into students (first_name,last_name,email) values ('Tom','Hanks','tom@cydeo.com');
 
