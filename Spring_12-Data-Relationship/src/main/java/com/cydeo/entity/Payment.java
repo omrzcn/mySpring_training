1028ca2b37041213e937ffaa834f3a2c01a3d182
omrzcn <omrzcnn@gmail.com>
2024-05-02 18:16:18 +0300

    table relationship

---
+++ Spring_12-Data-Relationship/src/main/java/com/cydeo/entity/Payment.java
@@ -0,0 +1,34 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Status;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name = "payments")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate createdDate;
+
+
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+
+
+
+}
