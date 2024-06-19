caa54ef221805212a10e3052945fbc4787b5b450
omrzcn <omrzcnn@gmail.com>
2024-06-19 15:43:58 +0300

    Api

--- spring_14_Data_Queries/src/main/java/com/cydeo/repository/CourseRepository.java
+++ spring_14_Data_Queries/src/main/java/com/cydeo/repository/CourseRepository.java
@@ -3,6 +3,8 @@ package com.cydeo.repository;
 import com.cydeo.entity.Course;
 import jdk.jfr.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.stream.Stream;
@@ -29,6 +31,9 @@ public interface CourseRepository extends JpaRepository<Course,Long> {
     Stream<Course> streamAllByCategory(String category);
 
 
+
+    @Query("select c from Course c where c.category = :category and c.rating> :rating")
+    List<Course>retrieveAllByCategoryAndRatingGreaterThan(@Param("category")String category,@Param("rating") int rating);
 
 
 
--- spring_15_REST/src/main/java/com/cydeo/controller/CourseController.java
+++ spring_15_REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -2,9 +2,7 @@ package com.cydeo.controller;
 
 import com.cydeo.dto.CourseDTO;
 import com.cydeo.service.CourseService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +21,40 @@ private final CourseService courseService;
 
         return courseService.getCourses();
     }
+
+
+    @GetMapping("{id}")
+    public CourseDTO getCourseById(@PathVariable("id") long courseId){
+
+        return courseService.getCourseById(courseId);
+    }
+
+
+    @GetMapping("/category/{name}")
+    public List<CourseDTO> getCoursesByCategory(@PathVariable("name") String category){
+
+        return courseService.getCoursesByCategory(category);
+    }
+
+
+
+    @PostMapping()
+    public CourseDTO createCourse(@RequestBody CourseDTO courseDTO){
+        return courseService.createCourse(courseDTO);
+    }
+
+
+    @PutMapping("{id}")
+    public void updateUser(@PathVariable("id") Long courseId, @RequestBody CourseDTO courseDTO){
+
+         courseService.updateCourse(courseId,courseDTO);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteCourseById(@PathVariable("id") Long courseId){
+        courseService.deleteCourseById(courseId);
+    }
+
 
 
 }
