3c470221f29c57dbb7adc918596bc04ba9b52313
omrzcn <omrzcnn@gmail.com>
2024-06-24 13:40:19 +0300

    restTemplate

---
+++ spring_16_REST_ConsumingAPIs/src/main/java/com/cydeo/controller/Customer_RestTemplate.java
@@ -0,0 +1,60 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.User;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+@RestController
+@RequestMapping("/cydeo")
+public class Customer_RestTemplate {
+
+    private final String URI="https://jsonplaceholder.typicode.com/users";
+    private final RestTemplate restTemplate;
+
+    public Customer_RestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+
+    @GetMapping
+    public ResponseEntity<User[]> readAllUsers(){
+        return restTemplate.getForEntity(URI, User[].class) ;
+
+    }
+
+
+
+    @GetMapping("{id}")
+    public Object readUser(@PathVariable("id") Integer id){
+
+        String URL = URI+"/{id}";
+
+        return restTemplate.getForObject(URL, Object.class,id);
+    }
+
+
+    @GetMapping("/test")
+    public ResponseEntity<Object> consumePostFromDummyApi(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.set("app-id","6298ebfecd0551211fce37a6");
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);   // here, we just set the headers. if we just wanna set headers we use exchange
+        return restTemplate.exchange("https://dummyapi.io/data/v1/user?limit=10", HttpMethod.GET,entity,Object.class); // if we want to set headers we use exchange.
+
+    }
+
+
+
+
+
+
+
+
+}
