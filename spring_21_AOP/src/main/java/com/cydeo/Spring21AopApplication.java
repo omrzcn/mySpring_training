8c6f9d3e75faa101f8495d71ae347cddccb32b25
omrzcn <omrzcnn@gmail.com>
2024-07-11 21:20:36 +0300

    Spring AOP

---
+++ spring_21_AOP/src/main/java/com/cydeo/Spring21AopApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring21AopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring21AopApplication.class, args);
+    }
+
+}
