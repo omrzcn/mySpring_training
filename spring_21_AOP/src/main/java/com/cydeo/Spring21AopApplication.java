bff496cb8d16a4a231c64dd5d5b13f793afd0d58
omrzcn <omrzcnn@gmail.com>
2024-07-11 23:27:26 +0300

    Spring AOP

--- spring_21_AOP/src/main/java/com/cydeo/Spring21AopApplication.java
+++ spring_21_AOP/src/main/java/com/cydeo/Spring21AopApplication.java
@@ -1,7 +1,9 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Spring21AopApplication {
@@ -10,4 +12,9 @@ public class Spring21AopApplication {
         SpringApplication.run(Spring21AopApplication.class, args);
     }
 
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }
