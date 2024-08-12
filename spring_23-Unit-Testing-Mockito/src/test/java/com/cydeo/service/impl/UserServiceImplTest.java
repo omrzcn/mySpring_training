56b05a92588b2157454b64b7112548aee2fad958
omrzcn <omrzcnn@gmail.com>
2024-08-12 13:43:52 +0300

    findByUsername method is tested and added more static methods

--- spring_23-Unit-Testing-Mockito/src/test/java/com/cydeo/service/impl/UserServiceImplTest.java
+++ spring_23-Unit-Testing-Mockito/src/test/java/com/cydeo/service/impl/UserServiceImplTest.java
@@ -4,6 +4,7 @@ import com.cydeo.mapper.UserMapper;
 import com.cydeo.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -34,13 +35,24 @@ class UserServiceImplTest {
         // checking the method runs or not.
         verify(userRepository).findByUserNameAndIsDeleted("harold@manager.com",false);
 
+
+        //times
         verify(userRepository,times(1)).findByUserNameAndIsDeleted("harold@manager.com",false);
 
+        //atLeastOnce()
         verify(userRepository,atLeastOnce()).findByUserNameAndIsDeleted("harold@manager.com",false);
 
+
+        //atLeast()
         verify(userRepository,atLeast(1)).findByUserNameAndIsDeleted("harold@manager.com",false);
         verify(userRepository,atMostOnce()).findByUserNameAndIsDeleted("harold@manager.com",false);
 
+
+        // InOrder
+        InOrder inOrder = inOrder(userRepository,userMapper);
+
+        inOrder.verify(userRepository).findByUserNameAndIsDeleted("harold@manager.com",false);
+        inOrder.verify(userMapper).convertToDto(null);
 
     }
 
